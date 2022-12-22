053d6845e422eaf799f6208c3113e36fba07e203
nghianguyenduc <nghia.nguyenduc@orientsoftware.com>
2022-12-22 11:03:41 +0700

    Update the process to pratice3 into session3 :coffee:

---
+++ p3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# 3. Write a Powershell script and a Bash script that:
+# - Take your input
+# - Check if the input is empty
+# - Create a new script (Check if the file exists) that prints out your
+# input and the new script name/full path
+# - Execute that new script
+
+read -p "Type your input: " input
+
+if [[ $input == "" ]]
+then 
+    echo "Your input is empty | Ending here"
+else
+    read -p "What script you want to excute, Remember give it to a file: " newscript
+    name_newfile=$(echo $newscript | cut -d ">" -f 2 | tr -d " ")
+    script=$(echo $newscript | cut -d ">" -f 1)
+    if [[ -f $name_newfile ]]
+    then
+        echo "file really exist !! Ending here"
+        exit 1
+    else
+        echo "The input is : $input"
+        pathoffile="$PWD/$name_newfile"
+        echo "Name: $name_newfile"
+        echo "Fullpath: $pathoffile"
+        echo "Script:" > $name_newfile
+        echo "$script" >> $name_newfile
+        echo "Result: " >> $name_newfile
+        command=$($script >> $name_newfile)
+        cat $name_newfile
+    fi
+fi
