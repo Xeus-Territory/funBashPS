9ee50baf6842b9b7d6785cba37d9f3445502183c
nghianguyenduc <nghia.nguyenduc@orientsoftware.com>
2022-12-23 14:53:50 +0700

    add p7 p8 for bash

--- p7.sh
+++ p7.sh
@@ -3,31 +3,17 @@
 #- Count “image” length
 #- Print an image name in the image list if the resolution is square
 
-filejson='{
-    "widget":{
-        "debug": "on",
-        "windows": {
-            "title": "SKW",
-            "name": "main_window",
-            "width": 500,
-            "height": 500
-        }
-    },
-    "Image":[
-        {
-            "src": "Images/Sun.png",
-            "name": "sun1",
-            "hoffset": 250,
-            "woffset": 250
-        },
-        {
-            "src": "Images/Sun2.png",
-            "name": "sun2",
-            "hoffset": 250,
-            "woffset": 240
-        }
+image_Length=$(cat image.json | jq '.Image | length')
+echo "The image Length: $image_Length"
+echo "Name the image is square shape: "
+for((i=0;i<$image_Length;i++)); do
+    o_hoffset=$(cat image.json | jq '.Image['$i'].hoffset')
+    o_woffset=$(cat image.json | jq '.Image['$i'].woffset')
+    if [[ $o_hoffset -eq $o_woffset ]]
+    then
+        echo $(cat image.json | jq '.Image['$i'].name')
+    else
+        continue
+    fi
+done
 
-    ]
-}'
-length_Image=$(echo $filejson | jq ".widget/Image")
-echo $length_Image
