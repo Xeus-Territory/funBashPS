35fb057bf8ae4af91cb4d4c52f9c8e7db7f4485b
ZnyGn <nguyen.nguyenhoaibao@orientsoftware.com>
2022-12-30 08:35:58 +0700

    Run compose

--- run_compose.sh
+++ run_compose.sh
@@ -1,17 +1,17 @@
+#!/bin/bash
+sudo docker pull mhart/alpine-node:12
+sudo docker build -t nodejs12:v12 ./Node_Hello_12/
 
-docker pull mhart/alpine-node:12
-docker build -t nodejs12:v12 ./Node_Hello_12/
+sudo docker pull mhart/alpine-node:14
+sudo docker build -t nodejs14:v14 ./Node_Hello_14/
 
-docker pull mhart/alpine-node:14
-docker build -t nodejs14:v14 ./Node_Hello_14/
-
-docker-compose up --detach
+sudo docker-compose up --detach
 
 
 echo "-----------------------------------------------"
 sleep 2
-test12=$(docker exec -it devops_orienttrainningsession_nodejs12_1 curl -I localhost:3000 | awk '/HTTP\// {print $2}')
-test14=$(docker exec -it devops_orienttrainningsession_nodejs14_1 curl -I localhost:3001 | awk '/HTTP\// {print $2}')
+test12=$(sudo docker exec -it devops_orienttrainningsession_nodejs12_1 curl -I localhost:3000 | awk '/HTTP\// {print $2}')
+test14=$(sudo docker exec -it devops_orienttrainningsession_nodejs14_1 curl -I localhost:3001 | awk '/HTTP\// {print $2}')
 if [[ $test12 == "200" ]]
 then 
     echo "The local web nodejs12 can accesible inside"
