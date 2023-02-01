a1f450d6cf3c932371006074547448400aff7e4e
nd-devops <nghia.nguyenduc@orientsoftware.com>
2023-02-01 10:19:04 +0700

    Network&&AutoScaling: Fix the issue connect from internet && assign architecture ASG for NSG to retricting the another scale set :coffee:

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -26,50 +26,65 @@ resource "azurerm_subnet" "my_terraform_network_subnet" {
 #   tags = local.common_tags
 # }
 
-# # Create a SecurityGroup
-# resource "azurerm_network_security_group" "my_terraform_nsg" {
-#   name                = "${local.environment}-nsg"
-#   location            = data.azurerm_resource_group.current.location
-#   resource_group_name = data.azurerm_resource_group.current.name
+resource "azurerm_application_security_group" "main" {
+  name = "${local.environment}-asg"
+  location = data.azurerm_resource_group.current.location
+  resource_group_name = data.azurerm_resource_group.current.name
+  tags = local.common_tags
+}
 
-#   security_rule {
-#     name                       = "SSH"
-#     priority                   = 1001
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "22"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
+# Create a SecurityGroup
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "${local.environment}-nsg"
+  location            = data.azurerm_resource_group.current.location
+  resource_group_name = data.azurerm_resource_group.current.name
 
-#   security_rule {
-#     name                       = "HTTP"
-#     priority                   = 1002
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "80"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
+  # security_rule {
+  #   name                       = "SSH"
+  #   priority                   = 1001
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "Tcp"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "22"
+  #   source_address_prefix      = "*"
+  #   destination_address_prefix = "*"
+  # }
 
-#   security_rule {
-#     name                       = "HTTPS"
-#     priority                   = 1003
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "443"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    # destination_address_prefix = "*"
+    destination_application_security_group_ids = [ azurerm_application_security_group.main.id ]
+  }
 
-#   tags = local.common_tags
-# }
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    # destination_address_prefix = "*"
+    destination_application_security_group_ids = [ azurerm_application_security_group.main.id ]
+  }
+
+  tags = local.common_tags
+}
+
+# Create association via subnet with nsg
+resource "azurerm_subnet_network_security_group_association" "main" {
+  subnet_id = azurerm_subnet.my_terraform_network_subnet.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
+}
 
 # # Create NIC
 # resource "azurerm_network_interface" "my_terraform_nic" {
@@ -117,13 +132,6 @@ resource "azurerm_public_ip" "publicip_LB" {
   location = data.azurerm_resource_group.current.location
   allocation_method = "Static"
   sku = "Standard"
-  tags = local.common_tags
-}
-
-resource "azurerm_application_security_group" "main" {
-  name = "${local.environment}-asg"
-  location = data.azurerm_resource_group.current.location
-  resource_group_name = data.azurerm_resource_group.current.name
   tags = local.common_tags
 }
 
