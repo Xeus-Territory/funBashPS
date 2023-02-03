89a84754c9db21e644c28f1143cb7c28bbed6bc5
nd-devops <nghia.nguyenduc@orientsoftware.com>
2023-02-03 09:25:03 +0700

    Resolved: Parameterized for bucket_name && split file to specify purpose file for each module :coffee:

--- terraform/modules/autoscaling/variables.tf
+++ terraform/modules/autoscaling/variables.tf
@@ -3,3 +3,8 @@ variable "email_notification" {
     type = string 
     sensitive = true
 }
+
+variable "bucket_name" {
+    type = string
+    description = "Name of the bucket to create"
+}
--- terraform/modules/bastion/network.tf
+++ terraform/modules/bastion/network.tf
@@ -12,15 +12,3 @@ resource "azurerm_subnet" "bastion_subnet" {
     virtual_network_name = data.azurerm_virtual_network.main.name
     address_prefixes = [ "10.0.2.0/29" ]
 }
-
-resource "azurerm_bastion_host" "main" {
-    name = "${local.environment}-bastionHost"
-    location = data.azurerm_resource_group.main.location
-    resource_group_name = data.azurerm_resource_group.main.name
-
-    ip_configuration {
-      name = "${local.environment}-bastionConfiguration"
-      subnet_id = azurerm_subnet.bastion_subnet.id
-      public_ip_address_id = azurerm_public_ip.bastion_public_ip.id
-    }
-}
