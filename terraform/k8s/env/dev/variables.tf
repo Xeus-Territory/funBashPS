e459343d4a20a9d3704e6d8dce5ab17f8c4d7b58
ZnyGn <nguyen.nguyenhoaibao@orientsoftware.com>
2023-02-20 10:19:44 +0700

    Update: Modified ingress-pipeline to not use input variable on pipeline

--- terraform/k8s/env/dev/variables.tf
+++ terraform/k8s/env/dev/variables.tf
@@ -7,9 +7,11 @@ variable "environment" {
 variable "cluster_name" {
     type = string
     description = "Name of the cluster"
+    default = "dev-k8s"
 }
 
 variable "cluster_resource_group" {
     type = string
     description = "Where the cluster is existence"
+    default = "dev-environment"
 }
