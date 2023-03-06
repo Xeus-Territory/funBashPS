8025603ffdb6779a84a5f75bcf717524c71163b0
nd-devops <nghia.nguyenduc@orientsoftware.com>
2023-03-06 10:33:53 +0700

    Modify: Revert a provider for pipeline

--- terraform/k8s/env/dev/providers.tf
+++ terraform/k8s/env/dev/providers.tf
@@ -1,18 +1,11 @@
 #Get the resource backend from the Private Storage backend
 terraform {
-  # backend "azurerm" {
-  #     resource_group_name = var.resourceGroup
-  #     storage_account_name = var.storageAccount
-  #     container_name = var.storageContainer
-  #     key = var.storageBlob
-  # }
-
-    backend "azurerm" {
-      resource_group_name = "DevOpsIntern"
-      storage_account_name = "orientdevopsintern"
-      container_name = "tfstate"
-      key = "monitoring.tfstate"
-    }
+  backend "azurerm" {
+      resource_group_name = var.resourceGroup
+      storage_account_name = var.storageAccount
+      container_name = var.storageContainer
+      key = var.storageBlob
+  }
   required_providers {
       azurerm = {
         source = "hashicorp/azurerm"
